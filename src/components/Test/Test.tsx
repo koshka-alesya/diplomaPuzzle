d7020fa8c2835d6ec5c545d67d2e60a2a237de5d
koshka-alesya <veronica.desmond@mail.ru>
2020-06-06 22:41:47 +0300

    test 2

--- src/components/Test/Test.tsx
+++ src/components/Test/Test.tsx
@@ -7,24 +7,55 @@ function Test() {
         const solveStates = game.solveA(3);
         console.log(solveStates);
     }
-    // @ts-ignore
-    function getTimeAlgorithms(test) {
-        const start = new Date().getTime();
-        test();
-        const end = new Date().getTime();
-        console.log('SecondWay: ${end - start}ms');
+    function testIDA(){
+        const game  = new Game(3);
+        const state = game.getState();
+        const solveStates = game.solveIDA(3);
+        console.log(solveStates);
     }
-    function mainTest() {
+    // @ts-ignore
+    function getTimeAlgorithms(alg: string) {
+        let min = 0;
+        let max = 0;
         const start = new Date().getTime();
-        testA();
+        if (alg === 'A') {
+            testA();
+        } else {
+            testIDA();
+        }
         const end = new Date().getTime();
-        const time = start - end;
-        console.log('Time:' + time + 'ms');
+        const time = end - start;
+        return time;
+    }
+    function mainTest(alg: string) {
+        let min = getTimeAlgorithms(alg);
+        let max = min;
+        let count = min;
+        for (let i = 1; i< 300; i++) {
+            let time = getTimeAlgorithms(alg);
+            if (time > max) {
+                max = time;
+            }
+            if (time < min) {
+                min = time;
+            }
+            count += time;
+        }
+        console.log('Среднее время: ' + count / 300 + 'ms');
+        console.log('Мин. время: ' + min + 'ms');
+        console.log('Макс. время: ' + max + 'ms');
     }
     return (
-        <div onClick={(e) => mainTest()}>
-                   test
+        <div>
+            <div onClick={(e) => mainTest('A')}>
+                test
+            </div>
+            <div onClick={(e) => mainTest('IDA')}>
+                test2
+            </div>
         </div>
+
+
     );
 }
 
